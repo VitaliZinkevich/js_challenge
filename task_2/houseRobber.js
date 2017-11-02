e98e417362f541899740d242bc4b09111dcf4d51
Vitali <31952551+VitaliZinkevich@users.noreply.github.com>
2017-11-02 18:04:12 +0300

    done
    максимальные члены массива не рядом

--- task_2/houseRobber.js
+++ task_2/houseRobber.js
@@ -1,3 +1,33 @@
 function houseRobber(nums) {
 
+        var max = nums[0];
+
+        var index=0
+
+
+        for (i = 1; i < nums.length; ++i) {
+          if (nums[i] > max) max = nums[i], index=i;
+
+        }
+
+        var max2 = nums[0];
+            for (i = 1; i < nums.length; ++i) {
+
+          if (nums[i] > max2 && max2 <= max) {
+
+              if (i == index || i == index-1 || i == index+1) {
+
+                  } else {
+
+                      max2 = nums[i]
+
+                      }
+
+              }
+
+        }
+
+        return max+max2
+
+
 }
